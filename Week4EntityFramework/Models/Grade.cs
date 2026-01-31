d5410a000483eebf4a015be15550ddd23c137886
Rozer Shrestha <rozer.shrestha611@gmail.com>
2026-01-31 19:20:32 +0545

    docx modifed

--- Week4EntityFramework/Models/Grade.cs
+++ Week4EntityFramework/Models/Grade.cs
@@ -2,10 +2,6 @@
 {
     public class Grade
     {
-        public Grade()
-        {
-            Students = new List<Student>();
-        }
         public int GradeId { get; set; }
         public string? GradeName { get; set; }
         public string? GradeStatus { get; set; }
